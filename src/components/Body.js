33e6265f957b98ac4ba9693831bc42c85fce66ae
RathodAnilT <132031578+RathodAnilT@users.noreply.github.com>
2024-08-16 19:52:20 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import {createBrowserRouter,  RouterProvider} from "react-router-dom";
+import Login from './Login'
+import Browse from './Browse'
+
+
+const Body = () => {
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login/>
+    },
+    {
+      path: "Browse",
+      element: <Browse/>
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router = {appRouter}/>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Browse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Browse = () => {
+    
+  return (
+    <div>
+      <h1>browse</h1>
+    </div>
+  )
+}
+
+export default Browse
